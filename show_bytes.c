b87caa2d5609737e0fecb5b44125f47f56e13e4b
Cale Switzer <audio_1@AUDIO-1.local>
2020-01-22 16:50:46 -0500

    working with pointers.

--- show_bytes.c
+++ show_bytes.c
@@ -8,7 +8,6 @@ void show_bytes(byte_pointer start, int len){
 		printf(" %.2x", start[i]);
 	}
 	printf("\n");
-
 }
 
 void show_int(int x){
@@ -23,13 +22,37 @@ void show_pointer(void *x){
 	show_bytes((byte_pointer) &x, sizeof(void *));
 }
 
-void show_char(char x){
-	show_bytes((byte_pointer) &x, sizeof(char));
+void show_short(short x){
+	show_bytes((byte_pointer) &x, sizeof(short));
 }
+
+void show_long(long x){
+	show_bytes((byte_pointer) &x, sizeof(long));
+}
+
+void show_double(double x){
+	show_bytes((byte_pointer) &x, sizeof(double));
+}
+
+int is_little_endian(){
+	short sample_byte = 1;
+	return sample_byte;	
+}
+
+void byte_mesh(int x, int y){
+	// combines least significant byte of x with remaining bytes of y
+	int *x_pointer = &x;
+	printf("%x\n", *x_pointer);
+}
+
 
 int main(){
 	show_int(255);
 	show_float(1.5);
-	show_char("a");
+	show_short(1);
+	show_long(32767);
+	show_double(32.32);
+	printf("%d\n", is_little_endian());
+	byte_mesh(0x89ABCDEF,2);
 	return 0;
 }
