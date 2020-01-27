fb8b63cee9bb827efb1bb9a3b8c37b08286e6142
Cale Switzer <audio_1@AUDIO-1.local>
2020-01-27 16:13:21 -0500

    2.60 completed.

--- show_bytes.c
+++ show_bytes.c
@@ -38,7 +38,7 @@ void show_double(double x){
 	show_bytes((byte_pointer) &x, sizeof(double));
 }
 
-////////// 2.55 //////////
+////////// 2.58 //////////
 
 int is_little_endian(){
 	short sample_byte = 1;
@@ -57,7 +57,9 @@ void byte_mesh(int x, int y){
 ////////// 2.60 //////////
 
 unsigned replace_byte(unsigned x, int i, unsigned char b){
-		
+	unsigned int x_masked = x & ~(0xFF << (8 * i));
+	unsigned int b_mask = b << (8 * i);	
+	return x_masked | b_mask;	
 }
 
 
@@ -69,6 +71,6 @@ int main(){
 	show_double(32.32);
 	printf("%d\n", is_little_endian());
 	byte_mesh(0x89ABCDEF, 0x76543210);
-	replace_byte(0x12345678, 2, 0xAB);
+	printf("%X\n", replace_byte(0x12345678, 0, 0xAB));
 	return 0;
 }
