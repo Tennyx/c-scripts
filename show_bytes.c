5755fcea317db6a707bc340dc2686e485ed2a149
Cale Switzer <audio_1@AUDIO-1.local>
2020-02-05 16:48:01 -0500

    finished problem 2.68.

--- show_bytes.c
+++ show_bytes.c
@@ -127,8 +127,36 @@ int odd_ones(unsigned x){
 	x ^= x >> 1;
 
 	return (x & 1);
-//	x &= 0xf;
-//	return (0x6996 >> x) & 1;
+}
+
+////////// 2.66 //////////
+
+int leftmost(unsigned x){
+	//bit smearing
+
+	x |= x >> 16;
+	x |= x >> 8;
+	x |= x >> 4;
+	x |= x >> 2;
+	x |= x >> 1;
+
+	return x ^= x >> 1;	
+}
+
+////////// 2.67 //////////
+
+int int_size_is_32(){
+	int set_msb = 1 << 15 << 15 << 1;
+  	int beyond_msb = set_msb << 1;
+	return set_msb && !beyond_msb;
+}
+
+////////// 2.68 //////////
+
+int lower_one_mask(int n){
+	int word_size = 8 * sizeof(int);
+	int val = (1 <<  (word_size - 1)) >> (word_size - 1);
+	return ~(val << n);		
 }
 
 int main(){
@@ -147,7 +175,10 @@ int main(){
 	printf("%d\n", srl(-2147483648,5));
 	printf("%d\n", sra(-2147483648,31));
 	printf("%d\n", any_odd_one(5));
-*/
 	printf("%d\n", odd_ones(8));
+	printf("%x\n", leftmost(0xFF00));
+	printf("%d\n", int_size_is_32());
+*/
+	printf("%x\n", lower_one_mask(17));
 	return 0;
 }
